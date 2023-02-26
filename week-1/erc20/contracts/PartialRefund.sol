1f37924a703f4479c998a5238a4ec2762b0dd0a6
Matt Zamora <mzamora2899@gmail.com>
2023-02-25 18:41:42 -0700

    fix bugs on partial refund

--- week-1/erc20/contracts/PartialRefund.sol
+++ week-1/erc20/contracts/PartialRefund.sol
@@ -5,16 +5,23 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract PartialRefund is ERC20 {
     address private owner = msg.sender;
-    uint256 maxSupply = 100000000;
-    uint256 private immutable eth = 1 * 10 ** 18;
+    uint256 maxSupply = 100000000 * (10 ** 18);
 
     constructor(uint256 initialSupply) ERC20("test", "TST") {
         _mint(msg.sender, initialSupply);
     }
 
-    function withdraw() external {
+    modifier onlyOwner() {
         require(msg.sender == owner, "only owner can withdraw");
+        _;
+    }
+
+    function withdrawEth() external onlyOwner {
         payable(owner).transfer(address(this).balance);
+    }
+
+    function withdrawTokens() external onlyOwner {
+        this.transfer(owner, balanceOf(address(this)));
     }
 
     function getContractBalance() external view returns (uint256) {
@@ -22,23 +29,30 @@ contract PartialRefund is ERC20 {
     }
 
     function createTokens() external payable {
-        require(msg.value >= eth, "Must send at least 1 ETH");
+        require(msg.value >= 1 ether, "Must send at least 1 ETH");
         require(totalSupply() <= maxSupply, "max supply reached");
-        _mint(msg.sender, 1000);
+        _mint(msg.sender, 1000 * (10 ** 18));
     }
 
-    function sellBack(uint256 amount) external returns (uint256) {
+    function sellBack(uint256 amount) external {
         require(balanceOf(msg.sender) >= amount, "Not enough tokens");
-
-        uint256 ethToReturn = ((amount / 1000) * eth) / 2;
         require(
-            address(this).balance >= eth / 2 &&
-                address(this).balance >= ethToReturn,
-            "Not enough ether in contract to sell"
+            address(this).balance >= 0.5 ether,
+            "Not enough eth in contract"
         );
-        approve(msg.sender, amount);
+        if (amount >= 1000 * (10 ** 18)) {
+            uint256 ethToTransfer = ((amount / (1000 * (10 ** 18))) *
+                (10 ** 18)) / 2;
+            require(
+                address(this).balance >= ethToTransfer,
+                "Not enough eth in contract sell 1000 less tokens"
+            );
+            payable(msg.sender).transfer(ethToTransfer);
+        }
         transferFrom(msg.sender, address(this), amount);
-        payable(msg.sender).transfer(ethToReturn);
-        return ethToReturn;
     }
+
+    receive() external payable {}
+
+    fallback() external payable {}
 }
--- week-1/erc20/contracts/TokenSale.sol
+++ week-1/erc20/contracts/TokenSale.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract PartialRefund is ERC20 {
+contract TokenSale is ERC20 {
     address private owner = msg.sender;
-    uint256 maxSupply = 100000000 * (10 ** 18);
+    uint256 maxSupply = 100000000;
 
     constructor(uint256 initialSupply) ERC20("test", "TST") {
         _mint(msg.sender, initialSupply);
@@ -21,26 +21,8 @@ contract PartialRefund is ERC20 {
     }
 
     function createTokens() external payable {
-        require(msg.value >= 1 ether, "Must send at least 1 ETH");
+        require(msg.value >= 1 * 10 ** 18, "Must send at least 1 ETH");
         require(totalSupply() <= maxSupply, "max supply reached");
         _mint(msg.sender, 1000);
     }
-
-    function sellBack(uint256 amount) external returns (uint256) {
-        require(balanceOf(msg.sender) >= amount, "Not enough tokens");
-
-        uint256 ethToReturn = ((amount / 1000) * 1 ether) / 2;
-        require(
-            address(this).balance >= 0.5 ether &&
-                address(this).balance >= ethToReturn,
-            "Not enough ether in contract to sell"
-        );
-        transferFrom(msg.sender, address(this), amount);
-        payable(msg.sender).transfer(ethToReturn);
-        return ethToReturn;
-    }
-
-    receive() external payable {}
-
-    fallback() external payable {}
 }
