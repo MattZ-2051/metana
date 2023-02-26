a37861c60e5e41890ded64890db7678e85786341
Matt Zamora <mzamora2899@gmail.com>
2023-02-25 19:06:55 -0700

    fix token sale

--- week-1/erc20/contracts/TokenSale.sol
+++ week-1/erc20/contracts/TokenSale.sol
@@ -5,24 +5,24 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TokenSale is ERC20 {
     address private owner = msg.sender;
-    uint256 maxSupply = 100000000;
+    uint256 maxSupply = 1000000000 * (10 ** 18);
 
     constructor(uint256 initialSupply) ERC20("test", "TST") {
         _mint(msg.sender, initialSupply);
     }
 
-    function withdraw() external {
+    function withdrawEth() external {
         require(msg.sender == owner, "only owner can withdraw");
         payable(owner).transfer(address(this).balance);
     }
 
-    function getContractBalance() external view returns (uint256) {
-        return address(this).balance;
+    function createTokens() external payable {
+        require(msg.value >= 1 ether, "Must send at least 1 ETH");
+        require(totalSupply() <= maxSupply, "max supply reached");
+        _mint(msg.sender, 1000 * (10 ** 18));
     }
 
-    function createTokens() external payable {
-        require(msg.value >= 1 * 10 ** 18, "Must send at least 1 ETH");
-        require(totalSupply() <= maxSupply, "max supply reached");
-        _mint(msg.sender, 1000);
-    }
+    receive() external payable {}
+
+    fallback() external payable {}
 }
