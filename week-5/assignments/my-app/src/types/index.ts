2da18103a46494eb6deafa8675b45d64e112aa47
Matt Zamora <mzamora2899@gmail.com>
2023-03-23 16:12:47 -0600

    add charts for block data

--- week-5/assignments/my-app/src/types/index.ts
+++ week-5/assignments/my-app/src/types/index.ts
@@ -1,4 +1,15 @@
-export type ChartLog = {
+export type TransferLogChart = {
   blockNumber: number;
   amount: number;
+};
+
+export type BaseFeeChart = {
+  blockNumber: number;
+  amount: number;
+};
+
+export type GasFeeChart = {
+  gasUsed: number;
+  gasLimit: number;
+  percentage: number;
 };
