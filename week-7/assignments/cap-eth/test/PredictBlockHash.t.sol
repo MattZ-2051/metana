9822233ce0480650ceb1e70baccfb1e35a27c0fd
Matt Zamora <mzamora2899@gmail.com>
2023-04-01 08:35:19 -0600

    add predict blockhash challenge

---
+++ week-7/assignments/cap-eth/test/PredictBlockHash.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+import "../src/PredictBlockHash.sol";
+
+contract PredictBlockHashTest is Test {
+    PredictTheBlockHashChallenge target;
+    address player = vm.addr(1);
+
+    function setUp() public {
+        target = new PredictTheBlockHashChallenge{value: 1 ether}();
+        vm.label(address(target), "Challenge");
+        vm.label(player, "Player");
+        vm.deal(player, 1 ether);
+    }
+
+    function testChallenge() public {
+        vm.startPrank(address(player));
+
+        target.lockInGuess{value: 1 ether}(bytes32(uint256(0)));
+        vm.roll(block.number + 1 + 257);
+        target.settle();
+
+        assertTrue(target.isComplete());
+    }
+}
