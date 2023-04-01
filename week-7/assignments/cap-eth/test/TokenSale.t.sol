96ec1824790f36fa5762d0aff84864f126ada8fd
Matt Zamora <mzamora2899@gmail.com>
2023-04-01 09:01:05 -0600

    add token sale contract and test, pending compiler version fix

---
+++ week-7/assignments/cap-eth/test/TokenSale.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.6;
+
+import "forge-std/Test.sol";
+import "../src/TokenSale.sol";
+
+contract TokenSaleTest is Test {
+    TokenSaleChallenge public target;
+    address public player;
+
+    function setUp() public {
+        target = new TokenSaleChallenge{value: 1 ether}();
+        vm.deal(player, 10 ether);
+    }
+
+    function testIsComplete() public {
+        vm.startPrank(address(player));
+        uint256 numTokens = type(uint256).max / 1e18 + 1;
+        target.buy{value: numTokens * 1e18}(numTokens);
+        uint256 balance = address(target).balance;
+        target.sell(balance / 1 ether);
+        assertTrue(target.isComplete());
+    }
+}
