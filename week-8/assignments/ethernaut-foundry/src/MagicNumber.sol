ef94a6d9b49f0f16a4d9b1ed2f1d0e7eca5a1cab
Matt Zamora <mzamora2899@gmail.com>
2023-04-14 12:34:12 -0600

    magic number solution

--- week-8/assignments/ethernaut-foundry/src/MagicNumber.sol
+++ week-8/assignments/ethernaut-foundry/src/MagicNumber.sol
@@ -1,6 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+contract Hack {
+    constructor(MagicNum target) {
+        address addr;
+        target.setSolver(addr);
+        bytes memory bytecode = hex"69602a60005260206000f3600052600a6016f3";
+        assembly {
+            // create(value, offset, size)
+            addr := create(0, add(bytecode, 0x20), 0x13)
+        }
+        require(addr != address(0));
+
+        target.setSolver(addr);
+    }
+}
+
 contract MagicNum {
     address public solver;
 
