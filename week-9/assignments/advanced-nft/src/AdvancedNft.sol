9b9f0b761fb5990887ffb90ffffe88c5da82d93b
Matt Zamora <mzamora2899@gmail.com>
2023-04-18 13:05:32 -0600

    wip

--- week-9/assignments/advanced-nft/src/AdvancedNft.sol
+++ week-9/assignments/advanced-nft/src/AdvancedNft.sol
@@ -8,6 +8,9 @@ import "@openzeppelin/contracts/utils/structs/BitMaps.sol";
 contract AdvancedNft is ERC721 {
     bytes32 public immutable root;
 
+    uint8 public max = 100;
+    mapping(address => Commit) public commits;
+
     constructor(
         string memory name,
         string memory symbol,
@@ -22,11 +25,17 @@ contract AdvancedNft is ERC721 {
         bool revealed;
     }
 
-    mapping(address => Commit) public commits;
+    function getHash(bytes32 data) public view returns (bytes32) {
+        return keccak256(abi.encodePacked(address(this), data));
+    }
 
-    function commit(bytes32 dataHash) public {
+    function commit(bytes32 dataHash, uint64 block_number) public {
+        require(
+            block_number > block.number,
+            "CommitReveal::reveal: Already revealed"
+        );
         commits[msg.sender].commit = dataHash;
-        commits[msg.sender].block = uint64(block.number);
+        commits[msg.sender].block = block_number;
         commits[msg.sender].revealed = false;
         emit CommitHash(
             msg.sender,
@@ -35,76 +44,35 @@ contract AdvancedNft is ERC721 {
         );
     }
 
-    event CommitHash(address sender, bytes32 dataHash, uint64 block);
-
     function reveal(bytes32 revealHash) public {
-        //make sure it hasn't been revealed yet and set it to revealed
         require(
             commits[msg.sender].revealed == false,
             "CommitReveal::reveal: Already revealed"
         );
-        commits[msg.sender].revealed = true;
-        //require that they can produce the committed hash
         require(
             getHash(revealHash) == commits[msg.sender].commit,
             "CommitReveal::reveal: Revealed hash does not match commit"
         );
-        //require that the block number is greater than the original block
-        require(
-            uint64(block.number) > commits[msg.sender].block,
-            "CommitReveal::reveal: Reveal and commit happened on the same block"
-        );
-        //require that no more than 250 blocks have passed
-        require(
-            uint64(block.number) <= commits[msg.sender].block + 250,
-            "CommitReveal::reveal: Revealed too late"
-        );
-        //get the hash of the block that happened after they committed
+
+        commits[msg.sender].revealed = true;
+
         bytes32 blockHash = blockhash(commits[msg.sender].block);
-        //hash that with their reveal that so miner shouldn't know and mod it with some max number you want
         uint8 random = uint8(
-            keccak256(abi.encodePacked(blockHash, revealHash))
+            uint(keccak256(abi.encodePacked(blockHash, revealHash)))
         ) % max;
         emit RevealHash(msg.sender, revealHash, random);
     }
 
     event RevealHash(address sender, bytes32 revealHash, uint8 random);
-
-    function getHash(bytes32 data) public view returns (bytes32) {
-        return keccak256(abi.encodePacked(address(this), data));
-    }
-
-    function revealAnswer(bytes32 answer, bytes32 salt) public {
-        //make sure it hasn't been revealed yet and set it to revealed
-        require(
-            commits[msg.sender].revealed == false,
-            "CommitReveal::revealAnswer: Already revealed"
-        );
-        commits[msg.sender].revealed = true;
-        //require that they can produce the committed hash
-        require(
-            getSaltedHash(answer, salt) == commits[msg.sender].commit,
-            "CommitReveal::revealAnswer: Revealed hash does not match commit"
-        );
-        emit RevealAnswer(msg.sender, answer, salt);
-    }
-
-    event RevealAnswer(address sender, bytes32 answer, bytes32 salt);
-
-    function getSaltedHash(
-        bytes32 data,
-        bytes32 salt
-    ) public view returns (bytes32) {
-        return keccak256(abi.encodePacked(address(this), data, salt));
-    }
+    event CommitHash(address sender, bytes32 dataHash, uint64 block);
 
     function redeem(
         address account,
-        uint256 bitMapIndex,
+        uint256 tokenId,
         bytes32[] calldata proof
     ) external {
         require(
-            _verify(_leaf(account, bitMapIndex), proof),
+            _verify(_leaf(account, tokenId), proof),
             "Invalid merkle proof"
         );
         _safeMint(account, tokenId);
