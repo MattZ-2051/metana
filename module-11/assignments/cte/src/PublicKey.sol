f63b0de4decb3582c87796f89b0c485ab0c96dcc
Matt Zamora <mzamora2899@gmail.com>
2023-05-01 16:38:58 -0600

    public key wip

---
+++ module-11/assignments/cte/src/PublicKey.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+contract PublicKeyChallenge {
+    address owner = 0x92b28647Ae1F3264661f72fb2eB9625A89D88A31;
+    bool public isComplete;
+
+    function authenticate(bytes memory publicKey) public {
+        require(address(keccak256(publicKey)) == owner);
+        isComplete = true;
+    }
+}
