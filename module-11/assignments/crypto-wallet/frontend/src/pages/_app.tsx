35c6e2e8c88b2e270e5362c22bfbd22fd609b18d
Matt Zamora <mzamora2899@gmail.com>
2023-05-03 11:36:43 -0600

    more setup

--- module-11/assignments/crypto-wallet/frontend/src/pages/_app.tsx
+++ module-11/assignments/crypto-wallet/frontend/src/pages/_app.tsx
@@ -1,8 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { configureChains, mainnet, createClient, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet],
+  [publicProvider()]
+);
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <Component {...pageProps} />
-  )
+    <WagmiConfig client={client}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  );
 }
